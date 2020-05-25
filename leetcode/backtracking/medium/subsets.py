b18d4c5c989cbe52838d5607b64f5b28637e2557
Howuhh <hsehowuhh@gmail.com>
2020-05-25 16:33:49 +0300

    subsets

--- leetcode/backtracking/medium/subsets.py
+++ leetcode/backtracking/medium/subsets.py
@@ -11,3 +11,33 @@ class Solution:
                 new_sets.append(sub + [num])
             power_set.extend(new_sets)
         return power_set
+
+    def subsets_rec(self, nums: List[int]) -> List[List[int]]:
+        power_set = []
+        
+        def gen_sub(start_index, curr_set):
+            power_set.append(curr_set)
+            
+            for k in range(start_index, len(nums)):
+                gen_sub(k + 1, curr_set + [nums[k]])
+                
+        gen_sub(0, [])
+        return power_set
+
+    def subsets_bits(self, nums: List[int]) -> List[List[int]]:
+        bitmasks = []
+        power_set = []
+        
+        def gen_masks(curr_mask=""):
+            if len(curr_mask) == len(nums):
+                bitmasks.append(curr_mask)
+                return
+                
+            gen_masks(curr_mask + "0")
+            gen_masks(curr_mask + '1')
+        
+        gen_masks()
+        for mask in bitmasks:
+            power_set.append([nums[i] for i in range(len(nums)) if mask[i] == "1"])
+            
+        return power_set
